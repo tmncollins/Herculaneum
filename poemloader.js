a1d1f72d849a5259ef98386e7f2ba91b66b37927
tmncollins <35235207+tmncollins@users.noreply.github.com>
2023-07-14 19:47:11 +0100

    Create poemloader.js

---
+++ poemloader.js
@@ -0,0 +1,32 @@
+// Reading data in utf-8 format
+// which is a type of character set.
+// Instead of 'utf-8' it can be
+// other character set also like 'ascii'
+
+function readTextFile(file)
+{
+    var rawFile = new XMLHttpRequest();
+    rawFile.open("GET", file, false);
+    var allText = "";
+    rawFile.onreadystatechange = function ()
+    {
+        if(rawFile.readyState === 4)
+        {
+            if(rawFile.status === 200 || rawFile.status == 0)
+            {
+                allText = rawFile.responseText;
+              
+            }
+        }
+    }
+    rawFile.send(null);
+    return allText;
+}
+
+function load_poem(title) 
+{
+	alert("Hello! I am an alert box!!");
+
+	var poem = readTextFile("poems/" + title + ".txt");
+    	document.getElementById("poem").innerHTML = poem;	
+}
